4c7df62398785999d17a48cb57284cefc2c0e203
Taylor Everts <tayloreverts@quickenloans.com>
2018-10-09 11:44:32 -0400

    actually this is the one. added some input validation

--- ConsoleApp4/ConsoleApp4/Program.cs
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -12,32 +12,48 @@ namespace ConsoleApp4
         {
             string name = "";
             int integer = 0;
+            string Continue = "y";
+
 
             Console.WriteLine("User! Enter your name: ");
             name = Console.ReadLine();
-            Console.WriteLine(name + "! Enter a number between 1 and 100, and perhaps I will be merciful.");
-            integer = int.Parse(Console.ReadLine());
 
-            if (integer > 0 && integer <= 100)
+            while (Continue == "y")
             {
-                if(integer % 2 != 0)
+                Console.WriteLine(name + "! Enter a number between 1 and 100, and perhaps I will be merciful.");
+                try
                 {
-                    Console.WriteLine(integer + " Odd");
+                    integer = int.Parse(Console.ReadLine());
                 }
-                if(integer % 2 == 0 && integer >= 2 && integer <= 25)
+                catch(FormatException)
                 {
-                    Console.WriteLine("Even and less than 25");
+                    Console.WriteLine("Why are you not entering numbers, mortal?");
+                    continue;
                 }
-                if(integer % 2 == 0 && integer >= 26)
+
+                if (integer > 0 && integer <= 100)
                 {
-                    Console.WriteLine(integer + " Even");
+                    if (integer % 2 != 0)
+                    {
+                        Console.WriteLine(integer + " Odd");
+                    }
+                    if (integer % 2 == 0 && integer >= 2 && integer <= 25)
+                    {
+                        Console.WriteLine("Even and less than 25");
+                    }
+                    if (integer % 2 == 0 && integer >= 26)
+                    {
+                        Console.WriteLine(integer + " Even");
+                    }
                 }
+                else
+                {
+                    Console.WriteLine(name + ", you have failed me. I asked for a specific number.");
+                }
+                Console.WriteLine("Shall we continue? (y/n)");
+                Continue = Console.ReadLine();
+                Continue.ToLower();
             }
-            else
-            {
-                Console.WriteLine(name + ", you have failed me. I asked for a specific number. I will now leave you.");
-            }
-            
 
             Console.WriteLine(name + ", press any key to exit");
             Console.ReadKey();
